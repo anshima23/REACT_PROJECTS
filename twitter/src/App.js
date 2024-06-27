5559f8c9f240d856653fad621429665778789a98
anshima23 <141510818+anshima23@users.noreply.github.com>
2024-06-27 19:34:56 +0530

    form

---
+++ twitter/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="logo-box">
+       <img src="./logo.png" alt="logo" className="logo"/>
+       <h2>Sign In to Twitter</h2>
+       <button>
+        <img src="./google.png" alt="logo"/>
+        Sign in with Google
+       </button>
+       <hr></hr>
+       <span>Or</span>
+
+       <form>
+        <input type="text" placeholder="Phone email or username"/>
+        <button>Next</button>
+       </form>
+
+       <button>Forget Password</button>
+       <p>Dont't have an account<a>Sign Up</a></p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
